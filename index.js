4d9a83e98a81194bd347ab6b69ebb6e94a9ff981
L-delara <ligia.delara15@gmail.com>
2023-04-30 15:40:16 -0400

    added places controller and 404

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const placesController = require("./controllers/places");
 
 const app = express();
 
@@ -7,5 +8,11 @@ app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Page</h1>");
+});
+
+//routes
+app.use("/breads", placesController);
 const PORT = process.env.PORT;
 app.listen(PORT, console.log(`listening on port ${PORT}`));
