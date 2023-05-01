ca2e5d0f94de2f9b13a8010eb67afd90e6d4357c
L-delara <ligia.delara15@gmail.com>
2023-05-01 17:35:27 -0400

    installed react and views

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express");
 require("dotenv").config();
-const placesController = require("./controllers/places");
-
+const express = require("express");
 const app = express();
 
+app.use("/places", require("./controllers/places"));
+
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Hello world! Welcome to home!");
 });
 
 app.get("*", (req, res) => {
@@ -13,7 +13,7 @@ app.get("*", (req, res) => {
 });
 
 //routes
-app.use("/places", placesController);
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, console.log(`listening on port ${PORT}`));
