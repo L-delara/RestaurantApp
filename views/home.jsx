10c5e8eb809b132e14cf3f74a5657d03e71fbbbc
L-delara <ligia.delara15@gmail.com>
2023-05-05 16:11:39 -0400

    add nav, style update
    up to 5,pt2

--- views/home.jsx
+++ views/home.jsx
@@ -8,26 +8,21 @@ function home() {
         <h1>HOME</h1>
         <div>
           <img
-            src="/images/pizza-margarita.jpg"
-            alt="cheese, tomato, and basil picture"
-            className="pizzaHome"
+            src="/images/ice-cream.jpg"
+            alt="vanilla and chocolate icecream cones"
+            className="foodHomeImg"
           />
           <div className="imgCredits">
             Photo by{" "}
-            <a href="https://unsplash.com/es/@innagurina?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-              {" "}
-              Inna Gurina
+            <a href="https://unsplash.com/pt-br/@littlebearhugs?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Donna G
             </a>{" "}
             on{" "}
-            <a href="https://unsplash.com/photos/JspLKUauwSI?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-              {" "}
+            <a href="https://unsplash.com/images/food/ice-cream?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
               Unsplash
             </a>
           </div>
         </div>
-        <a href="/places">
-          <button className="btn-primary">Places Page</button>
-        </a>
       </main>
     </Def>
   );
